48e035823bc193bd39892789708773094b9b5d89
Hackathon <sophia.karski@db.com>
2022-09-07 22:04:30 +0100

    first commit on this repo

--- MqLens/ContentView.swift
+++ MqLens/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world! and all the rest ;-)")
             .padding()
     }
 }
