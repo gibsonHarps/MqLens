434d9e26c1c1a035048c42c7475d0736805486ae
Hackathon <sophia.karski@db.com>
2022-09-07 21:56:26 +0100

    Initial Commit

---
+++ MqLens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MqLens
+//
+//  Created by hackathon on 07/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MqLens/MqLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MqLensApp.swift
+//  MqLens
+//
+//  Created by hackathon on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct MqLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
